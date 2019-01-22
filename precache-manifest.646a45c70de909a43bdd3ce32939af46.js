7f7a2b09d2d021a2a8411dc099139d8dcc1a1c5b
Shaun Yong <shaunyong9@gmail.com>
2019-01-22 23:33:50 +0800

    Updates

---
+++ precache-manifest.646a45c70de909a43bdd3ce32939af46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/youtube-frontend-bootstrap/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "58360f6633853bf35f00",
+    "url": "/youtube-frontend-bootstrap/static/js/runtime~main.58360f66.js"
+  },
+  {
+    "revision": "37bd27f0717b0c4c2e5c",
+    "url": "/youtube-frontend-bootstrap/static/js/main.37bd27f0.chunk.js"
+  },
+  {
+    "revision": "0389cfca216f9d9ed034",
+    "url": "/youtube-frontend-bootstrap/static/js/1.0389cfca.chunk.js"
+  },
+  {
+    "revision": "37bd27f0717b0c4c2e5c",
+    "url": "/youtube-frontend-bootstrap/static/css/main.77046f42.chunk.css"
+  },
+  {
+    "revision": "3eef48ab1023c1858e63569697cc8aa4",
+    "url": "/youtube-frontend-bootstrap/index.html"
+  }
+];
